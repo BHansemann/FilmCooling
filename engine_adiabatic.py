2736f018547903aedd58446ebb0f193720e86976
Bernhard Hansemann <bernhard.hansemann@gmail.com>
2020-09-25 17:51:23 +0200

    Added rocketcea

--- engine_adiabatic.py
+++ engine_adiabatic.py
@@ -9,12 +9,15 @@ import CoolProp.CoolProp as CP
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from rocketcea.cea_obj import CEA_Obj
 
 def engine_adiabatic(P_cc, P_e, n_ps, T_cc, F_th, eta_nz, ox, fuel, ofr):
     P, T, rho, kappa, h, u, c, M, r = 1, 2, 3, 4, 5, 6, 7, 8, 9
+    cea = CEA_Obj(fuelName=fuel, oxName=ox)
     eng = np.zeros((10, n_ps + 1))
     eng[0,:] = np.arange(0, n_ps + 1)
     eng[P,:] = P_cc - ((P_cc - P_e)/n_ps) * eng[0,:]
+    eng[T;0] = cea.get_Temperatures(pc_units='bar', PC=P_cc, MR=orf)[0]
     
     print(eng[P,:])
     
