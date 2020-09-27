8984790b4bc41f29f91161b8aa1c4edbbfcf4d94
Bernhard Hansemann <bernhard.hansemann@gmail.com>
2020-09-27 18:30:50 +0200

    Added combustion products

--- engine_adiabatic.py
+++ engine_adiabatic.py
@@ -10,18 +10,33 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from rocketcea.cea_obj import CEA_Obj
+import scipy.constants as const
+
+psi = lambda x : x / const.psi
+Pa = lambda x : x * const.psi
+
+def convert_to_CP_string(cea_dict, cutoff=0.01, exclude=["*NO", "*O", "*OH"], pos=1):
+    cp_string = ""
+    cp_dict = {}
+    total = 0
+    for key in cea_dict:
+        if (cea_dict[key][pos] > cutoff) and not (key in exclude):
+            cp_dict[key.strip('*')] = cea_dict[key][pos]
+            total += cea_dict[key][pos]
+    for key in cp_dict:
+        cp_string += "{}[{}]&".format(key, cp_dict[key]/total)
+    return cp_string.strip("&")
 
 def engine_adiabatic(P_cc, P_e, n_ps, T_cc, F_th, eta_nz, ox, fuel, ofr):
-    P, T, rho, kappa, h, u, c, M, r = 1, 2, 3, 4, 5, 6, 7, 8, 9
+    step, P, T, rho, kappa, h, u, c, M, r = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
     cea = CEA_Obj(fuelName=fuel, oxName=ox)
-    cea.setupCards(pc_units='bar', Pc=P_cc*10**-5, MR=ofr)
     eng = np.zeros((10, n_ps + 1))
-    eng[0,:] = np.arange(0, n_ps + 1)
+    eng[step,:] = np.arange(0, n_ps + 1)
     eng[P,:] = P_cc - ((P_cc - P_e)/n_ps) * eng[0,:]
-    eng[T,0] = cea.get_Temperatures(Pc=P_cc, MR=ofr)[0]
+    eng[T,0] = cea.get_Temperatures(Pc=psi(P_cc), MR=ofr)[0]
+    mix_cc = convert_to_CP_string(cea.get_SpeciesMoleFractions(Pc=psi(P_cc), MR=ofr)[1])
     
-    print(eng[T,0])
+    #print(cea.get_Temperatures(Pc=psi(P_cc), MR=ofr))
     
 if __name__ == "__main__":
     sample_engine = engine_adiabatic(1000000, 100000, 100, 0,0,0,"N2O","C2H5OH",3)
-
