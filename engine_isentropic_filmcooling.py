93df4145b12e3e699f09557d494a58e5ded69140
BHansemann <bernhard.hansemann@gmail.com>
2021-07-26 09:24:59 +0200

    Annotated

--- engine_isentropic_filmcooling.py
+++ engine_isentropic_filmcooling.py
@@ -112,7 +112,7 @@ while True:
                 P_2, T_2, u_2 = tuple(vec)
                 return np.array([P_2 * u_2 * area(s1[r]) / (T_2 * Rs) - s1[rho] * s1[u] * area(s1[r]) - s1[Q] / h_ev,
                                  P_2 * u_2 * area(s1[r]) * s2[h] / (T_2 * Rs) - s1[rho] * s1[u] * area(s1[r]) + s1[h] - mdotf * h_f + s1[Q], #Massenstrom vom film cooling berechnen
-                                 P_2 * u_2**2 * area(s1[r]) / (T_2 * Rs) + P_2 * area(s1[r]) - s1[rho] * s1[u]**2 * area(s1[r]) - s1[P] * area(s1[r]) + Fr])
+                                 P_2 * u_2**2 * area(s1[r]) / (T_2 * Rs) + P_2 * area(s1[r]) - s1[rho] * s1[u]**2 * area(s1[r]) - s1[P] * area(s1[r]) + Fr]) #check this
             
             def jacobimatrix(vec):
                 P_2, T_2, u_2 = tuple(vec)
@@ -127,7 +127,7 @@ while True:
                                  alpha * P_2 * s2[h] / u_2
                                  ],
                                 
-                                [alpha * u_2 + area(s2[r]),
+                                [alpha * u_2 + area(s2[r]), #check this pls
                                  alpha * -1 * P_2 * u_2,
                                  alpha * 2 * P_2
                                 ]])
